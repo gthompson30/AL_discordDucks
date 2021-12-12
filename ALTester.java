c194df437020eb1acccecc6d392a147f9c9f2643
Gabriel Thompson <gthompson30@stuy.edu>
2021-12-12 15:23:47 -0500

    weekend work for lab02 (work part I)

--- ALTester.java
+++ ALTester.java
@@ -1,12 +1,33 @@
+import java.util.*;
+
 public class ALTester {
 
-	private ArrayList _data = new ArrayList();
+	public static ArrayList _data = new ArrayList();
 
 	public static void main(String[] args) {
 		for (int i = 0; i < 23; i++)
-			_data.add(i);
+			// adds numbers in descending order, but it should still be added in the correct place
+			OrderedArrayList.add(_data, 23 - i);
 
-		for (int i = 0; i < _data.size(); i++)
-			System.out.print(_data.get(i) + ",");
+		// should be sorted #'s 1-23
+		printArrayList(_data);
+
+		// should be true
+		System.out.println(isSorted(_data));
+	}
+
+	public static boolean isSorted(ArrayList<Integer> a) {
+		for (int i = 0; i < a.size() - 1; i++) {
+			if (a.get(i) > a.get(i + 1))
+				return false;
+		}
+		return true;
+	}
+
+	public static void printArrayList(ArrayList<Integer> a) {
+		System.out.print("{");
+		for (int i = 0; i < a.size() - 1; i++)
+			System.out.print(a.get(i) + ", ");
+		System.out.println(a.get(a.size() - 1) + "}");
 	}
 }
--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -2,15 +2,37 @@ import java.util.*;
 
 public class OrderedArrayList {
 
-	public boolean add(ArrayList<Integer> a, int n) {
+	public static boolean add(ArrayList<Integer> a, int n) {
+		if (a.size() == 0) {
+			a.add(n);
+			return true;
+		}
 		int index = 0;
 
-		while (n < a.get(index)) {
+		while (index < a.size()) {
+			if (a.get(index) > n) {
+				a.add(index, n);
+				return true;
+			}
 			index++;
 		}
 
-		System.out.println(index);
+		a.add(n);
 		return true;
+	}
+
+	public static boolean remove(ArrayList<Integer> a, int n) {
+		for (int i = 0; i < a.size(); i++) {
+			if (n == a.get(i)) {
+				a.remove(i);
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static int size(ArrayList<Integer> a) {
+		return a.size();
 	}
 
 }
